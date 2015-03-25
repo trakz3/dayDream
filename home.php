247722da3d6b2f4b645a74b2b5cfa3133dd5dfe1
Aex Q <quesadalex@gmail.com>
2015-03-25 16:18:00 -0700

    added video

--- home.php
+++ home.php
@@ -41,7 +41,6 @@
                 <img class="img-responsive" src="img/DreamDay.png" alt="">
             </div>
             <div class="col-lg-7">
-
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Agf-ViFk_ac"></iframe>
                 </div>
