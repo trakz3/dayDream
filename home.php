4b8a11cb326cf875be8b9f96286c5ea4c8ecd5af
Aex Q <quesadalex@gmail.com>
2015-03-25 16:17:34 -0700

    updated the js url

--- home.php
+++ home.php
@@ -37,9 +37,14 @@
 <header>
     <div class="container">
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-lg-5">
                 <img class="img-responsive" src="img/DreamDay.png" alt="">
             </div>
+            <div class="col-lg-7">
+
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Agf-ViFk_ac"></iframe>
+                </div>
         </div>
     </div>
 </header>
